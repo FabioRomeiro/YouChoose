55f272c3df67cd3200f0af91f5d9c1f700d59fdc
FabioRomeiro <flromeiroc@gmail.com>
2019-07-15 01:08:24 -0300

    Making development easier with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,68 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('server', () => {
+gulp.task('clean', () =>
+  gulp.src('dist')
+    .pipe(clean())
+);
+
+gulp.task('copy', ['clean'], () =>
+  gulp.src([
+    './src/**/*',
+    '!./src/{sass,sass/**}',
+    '!./src/{ts,ts/**}'
+  ])
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('imagemin', () => {
+  gulp.src('src/assets/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/assets/images'));
+});
+
+gulp.task('fonts', () => {
+  gulp.src('src/assets/fonts/**/*')
+    .pipe(gulp.dest('dist/assets/fonts'));
+});
+
+gulp.task('sass', () =>
+  gulp.src('src/sass/styles.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'))
+);
+
+gulp.task('ts', () =>
+  tsProject.src()
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest('dist/js/'))
+);
+
+gulp.task('build', ['copy'], () => {
+  gulp.start('sass','ts', 'imagemin', 'fonts');
+});
+
+gulp.task('default', ['build']);
+
+gulp.task('server', ['sass', 'ts'], () => {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: 'src'
     }
   });
+
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/ts/**/*.ts', ['ts']);
 
   gulp.watch('src/**/*').on('change', browserSync.reload);
 });
