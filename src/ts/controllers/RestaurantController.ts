ea553bfae99fa845481f882c5898aa605de8e1b5
FabioRomeiro <flromeiroc@gmail.com>
2019-07-16 03:03:11 -0300

    Implementing Editor's HTML and CSS based on the Design

--- src/ts/controllers/RestaurantController.ts
+++ src/ts/controllers/RestaurantController.ts
@@ -6,6 +6,7 @@ export class RestaurantController {
   private _inputName: HTMLInputElement;
   private _inputPrice: HTMLInputElement;
   private _inputId: HTMLInputElement;
+  private _priceDisplay: Element;
 
   private _restaurants = new Restaurants();
   private _restaurantsView = new RestaurantsView('[data-options-list]');
@@ -15,14 +16,20 @@ export class RestaurantController {
     this._inputName = <HTMLInputElement>document.querySelector('[data-form-input=name]');
     this._inputPrice = <HTMLInputElement>document.querySelector('[data-form-input=price]');
     this._inputId = <HTMLInputElement>document.querySelector('[data-form-input=id]');
+    
+    this._priceDisplay = document.querySelector("[data-price-display]");
 
     this._restaurantsView.update(this._restaurants);
+
+    this._inputPrice.oninput = this._updatePriceSlider.bind(this);
+    this._updatePriceSlider();
   }
 
   private cleanUpFields(): void {
     this._inputName.value = "";
-    this._inputPrice.value = "";
+    this._inputPrice.value = "0";
     this._inputId.value = "";
+    this._updatePriceSlider();
   }
 
   private isFilled() {
@@ -57,7 +64,13 @@ export class RestaurantController {
     this._restaurantsView.update(this._restaurants);
   }
 
+  private _updatePriceSlider(): void {
+    this._inputPrice.setAttribute("value", this._inputPrice.value);
+    this._priceDisplay.innerHTML = '$'.repeat(parseInt(this._inputPrice.value));
+  }
+
   addMode(event: Event): void {
+    
     event.preventDefault();
 
     this._editorModeView.activateAddMode();
